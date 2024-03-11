9d0a403ee7418f4cabe01e79ac9c0e11f774b962
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-11 23:21:24 +0530

    Initial commit
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hangman",
+  description:
+    "Join the fun game of hangman and play with you friends for FREE!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
